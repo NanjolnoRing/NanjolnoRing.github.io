8ebb0689563c446dc92e902b9d9f02afa3fb8ba6
niking0210 <chengningkk@gmail.com>
2024-10-14 11:55:00 +0800

    Site updated: 2024-10-14 11:54:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/14/搭建Hexo博客/","2024/10/12/hello-world/","2024/10/14/这是一另篇新的博文/","2024/10/12/这是一篇新的博文/","2024/10/14/美化Hexo博客/"];function toRandomPost(){
+var posts=["2024/10/12/hello-world/","2024/10/14/搭建Hexo博客/","2024/10/14/这是一另篇新的博文/","2024/10/14/美化Hexo博客/","2024/10/12/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
